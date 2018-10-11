32fd4dcf49a08f92fc0057c419589846f4204fe1
minwoo.kim <minu1540@gmail.com>
2018-10-11 14:30:40 +0900

    [new] vue 모바일 청첩장 프로젝트 생성

---
+++ wedding/src/components/app/js/app.js
@@ -0,0 +1,17 @@
+import Intro from '../../intro/intro.vue'
+
+export default {
+    name: 'app',
+    components: {
+        Intro
+    },
+    data() {
+        return {
+        };
+    },
+    methods : {
+    },
+    created() {
+        return;
+    },
+}
---
+++ wedding/src/components/intro/intro.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="intro-container">
+
+        <div class="intro-main-img"></div>
+
+        <div class="intro-into-area white-bg">
+            <div class="intro-name-box">
+                <div class="intro-name-bundle">
+                    <img class="intro-name-box-fingerprint-img" src="../../assets/images/fingerprint.png" />
+                    <span class="intro-name left">송중기</span>
+                    <span class="intro-name right">송혜교</span>
+                </div>
+            </div>
+
+            <div class="intro-date">
+                2018. 10. 10 Sat PM 12:00
+            </div>
+
+            <div class="intro-location">
+                동해물과백두산이
+            </div>
+        </div>
+    </div>
+</template>
+
+<style type="text/css" src="./css/intro.css"></style>
