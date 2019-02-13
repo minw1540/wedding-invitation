3c59ff0d154cfbab676841f1cd5779f975cb5313
minwoo.kim <minu1540@gmail.com>
2019-02-13 17:56:49 +0900

    [mod] map 코드 수정

--- wedding/src/components/map/js/map.js
+++ wedding/src/components/map/js/map.js
@@ -14,30 +14,32 @@ export default {
 				let container = this.$refs.mapCanvas;
 				let options = {
 					center: new daum.maps.LatLng(37.52182734498564, 126.91917111126821),
-					level: 3
+					level: 4,
 				};
 
+				//맵 생성
 				let map = new daum.maps.Map(container, options);
 
-				// 주소-좌표 변환 객체를 생성합니다
-				var geocoder = new daum.maps.services.Geocoder();
+				// 주소-좌표 변환 객체를 생성
+				let geocoder = new daum.maps.services.Geocoder();
 
-				// 주소로 좌표를 검색합니다
-				geocoder.addressSearch('여의도동 28-2', function(result, status) {
-					// 정상적으로 검색이 완료됐으면
-					if (status === daum.maps.services.Status.OK) {
-
-						var coords = new daum.maps.LatLng(result[0].y, result[0].x);
-
-						// 결과값으로 받은 위치를 마커로 표시합니다
-						var marker = new daum.maps.Marker({
-							map: map,
-							position: coords
-						});
-
-						// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-						map.setCenter(coords);
+				// 주소로 좌표를 검색
+				geocoder.addressSearch('여의도동 28-2', (result, status) => {
+					// http://map.daum.net/link/search/여의도웨딩컨벤션
+					if (status !== daum.maps.services.Status.OK) {
+						return;
 					}
+
+					let coords = new daum.maps.LatLng(result[0].y, result[0].x);
+
+					//마커 표시
+					let marker = new daum.maps.Marker({
+						map: map,
+						position: coords,
+					});
+
+					//지도 센터 이동
+					map.setCenter(coords);
 				});
 				return;
 			},
